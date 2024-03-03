23bf97d727c08e255318c5e33a8a3895e0f26ea9
Shinya Fujino <shf0811@gmail.com>
2024-03-03 21:49:31 +0900

    Add theme-color

--- routes/_app.tsx
+++ routes/_app.tsx
@@ -7,6 +7,7 @@ export default function App({ Component }: PageProps) {
       <head>
         <meta charset="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta name="theme-color" content="#86efac" />
         <link rel="stylesheet" href="/styles.css" />
       </head>
       <body class="mx-auto md:p-2 md:w-[85%] bg-[#f9f9f9]">
